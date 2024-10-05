a1c40a55387e53fa26fad7259a79abe2d60bbe9e
JianYuChentw <coke818572@gmail.com>
2024-10-05 11:15:23 +0800

    忽略 config.js 文件

--- view/js/config.js
+++
@@ -1,9 +0,0 @@
-const baseUrl = 'http://localhost:3100/api2'; // 伺服器基礎 URL
-// const baseUrl = ''; // AWS雲端伺服器基礎 URL
-// const baseUrl = ''; // GCP雲端伺服器基礎 URL
-
-const sockUrl = 'ws://localhost:3100'; // 伺服器socket URL
-// const sockUrl = ''; // AWS雲端伺服器socket URL
-// const sockUrl = ''; // GCP雲端伺服器socket URL
-
-
